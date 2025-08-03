6202c4031591c25160dac7a196a1c68d7c514751
nihal-soni <nihalsoni0523@gmail.com>
2025-08-03 14:25:40 +0530

    feat: added recct-router-dom and signin/signup pages

---
+++ src/auth/sign-in/sign-in.tsx
@@ -0,0 +1,8 @@
+
+const SignInPage = () => {
+  return (
+    <div>SignIn</div>
+  )
+}
+
+export default SignInPage
--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,37 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import SignInPage from './auth/sign-in/sign-in.tsx'
+import { ThemeProvider } from './context/theme-provider.tsx'
+
+import Home from './home/home.tsx'
+import Dashboard from './dashboard/dashboard.tsx'
+
+const router = createBrowserRouter([
+   {
+    element: <App />,
+    children: [
+      {
+        path: '/',
+        element: <Home />
+      }, 
+      {
+        path: '/dashboard',
+        element: <Dashboard />
+      }
+    ]
+  },
+  {
+    path: 'auth/sign-in',
+    element: <SignInPage />
+  }
+])
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <ThemeProvider>
+      <RouterProvider router={router} />
+    </ThemeProvider>
   </StrictMode>,
 )
